4e9ccea9e28c96a9c499b84708e6acf387f385b1
Sachin Malhotra <sachinma@usc.edu>
2018-03-31 02:05:46 -0700

    Updated solution.py with comments

--- Adhoc/Task-Scheduler/solution.py
+++ Adhoc/Task-Scheduler/solution.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 import heapq
 from collections import Counter
 
@@ -10,11 +7,16 @@ class Solution:
         """
         :type tasks: List[str]
         :type n: int
-        :rtype: int
         """
 
+        """ 
+            This will count the frequency of individual tasks 
+            in the list of given tasks and provide a dictionary 
+            for the same 
+        """
         freq = Counter(tasks)
 
+        """ Create a Max Heap using the frequencies. We negate the values so as to get the max heap property. """
         heaplist = [(-freq[key], key) for key in freq]
         heapq.heapify(heaplist)
 
@@ -22,26 +24,40 @@ class Solution:
         steps = 0
         is_non_zero = False
 
+        """ Continue until the heap is not empty.  """
         while heaplist:
             i = 0
 
+            """ Pop until the heap finishes or until n elements have been popped. n being the interval """
             while i <= n:
                 if heaplist:
-                    (frequency, character) = heapq.heappop(heaplist)
+                    """ 
+                        pop an element from the heap 
+                        and add to a temporary list 
+                        with updated frequency. Note 
+                        the addition because we took 
+                        negative frequency values initially 
+                    """
+                    frequency, character = heapq.heappop(heaplist)
                     if frequency + 1 < 0:
+                        """ boolean variable to tell us if any non zero element is left or not. If not, then we are done """
                         is_non_zero = True
                     interim_list.append((frequency + 1, character))
                 else:
                     break
                 i += 1
-            steps += (n + 1 if is_non_zero else i)
+            """ Note, the number of steps is always (n+1) except in the last stretch.  """
+            steps += n + 1 if is_non_zero else i
 
+            """ 
+                Push elements from interim list 
+                back to the heap. Note that we 
+                only push the elements back if 
+                they have non zero freequency.  
+            """
             for element in interim_list:
                 if element[0] < 0:
                     heapq.heappush(heaplist, element)
             interim_list = []
             is_non_zero = False
         return steps
-
-
-
