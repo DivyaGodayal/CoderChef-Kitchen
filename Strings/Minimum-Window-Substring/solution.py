f80146fd3066408cedc7b496206f283a78bdceff
Sachin Malhotra <sachinma@usc.edu>
2018-08-23 00:19:31 -0700

    Update solution.py

--- Strings/Minimum-Window-Substring/solution.py
+++ Strings/Minimum-Window-Substring/solution.py
@@ -12,21 +12,25 @@ class Solution(object):
         
         # Dictionary which keeps a count of all the unique characters in t.
         dict_t = Counter(t)
+        
         # Number of unique characters in t, which need to be present in the desired window.
         required = len(dict_t)
     
-        # Left pointer
-        l = 0
-        # Right pointer
-        r = 0 
+        # left and right pointer
+        l, r = 0, 0
+        
         # formed is used to keep track of how many unique characters in t are present in the current window in its desired frequency.
-        # Foreg. if t is "AABC" then the window must have two A's, one B and one C. Thus formed would be = 3 when all these conditions are met.
+        # e.g. if t is "AABC" then the window must have two A's, one B and one C. Thus formed would be = 3 when all these conditions are met.
         formed = 0
+        
         # Dictionary which keeps a count of all the unique characters in the current window.
         window_counts = {}
+        
         # ans tuple of the form (window length, left, right)
         ans = float("inf"), None, None
+        
         while r < len(s):
+            
             # Add one character from the right to the window
             character = s[r]
             window_counts[character] = window_counts.get(character, 0) + 1
@@ -35,16 +39,22 @@ class Solution(object):
             if character in dict_t and window_counts[character] == dict_t[character]:
                 formed += 1
             
+            # Try and contract the window till the point where it ceases to be 'desirable'.
             while l <= r and formed == required:
                 character = s[l]
+                
                 # Save the smallest window until now.
                 if r - l + 1 < ans[0]:
                     ans = (r - l + 1, l, r)
-                # Move the left pointer ahead, this would help to look for a new window.
+                    
+                # The character at the position pointed by the `left` pointer is no longer a part of the window.
                 window_counts[character] -= 1
                 if character in dict_t and window_counts[character] < dict_t[character]:
                     formed -= 1
-                l += 1    
                 
+                # Move the left pointer ahead, this would help to look for a new window.
+                l += 1    
+            
+            # Keep expanding the window once we are done contracting.
             r += 1    
         return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]    
