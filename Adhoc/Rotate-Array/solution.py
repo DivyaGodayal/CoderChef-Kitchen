d14ab3e31b77785c8109febe9fbc59740663ad09
Samhitha Rama Prasad <46335615+samhitharamaprasad@users.noreply.github.com>
2019-01-06 02:13:49 -0800

    Add files via upload

---
+++ Adhoc/Rotate-Array/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        length=len(nums)
+        k=k%length
+        self.reverse(nums, 0, length-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, length-1)
+           
+    def reverse(self, nums, start, end):
+        while(start<end):
+            nums[start], nums[end]=nums[end], nums[start]
+            start=start+1
+            end=end-1
+            
