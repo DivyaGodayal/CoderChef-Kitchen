bac8b23fb1a852537d54d86aa2240219e56366cb
ankit <ankitchachada@gmail.com>
2019-08-06 21:58:15 -0700

    fizzbuzz in ruby

---
+++ Adhoc/Fizz-Buzz/solution_string_concat.rb
@@ -0,0 +1,20 @@
+class Solutuon
+	def fizz_buzz(n)
+		str = ""
+    (1..n).each do |i|
+        if i%15 == 0
+          str += "FizzBuzz"
+        elsif i%5 == 0
+          str += "Buzz"
+        elsif i%3 == 0
+          str += "Fizz"
+        else
+          str += i.to_s
+        end
+    end
+    puts str
+	end
+end
+
+Solutuon.new.fizz_buzz(15)
+
